f3926efa1dd9c6457574e6432a04e33ce391e71b
Andrew <retsediv1@gmail.com>
2019-11-29 10:30:07 +0200

    Fix get_length function

--- christofides.py
+++ christofides.py
@@ -44,7 +44,7 @@ def tsp(data):
 
 
 def get_length(x1, y1, x2, y2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1.0 / 2.0)
 
 
 def build_graph(data):
